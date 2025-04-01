a0f365dffed25b37c68c6c7b53d9788395c522ea
webpagearshi <arshisaleh@gmail.com>
2025-04-01 17:14:41 +0530

    add table

--- dom_selection_example.js
+++ dom_selection_example.js
@@ -1,4 +1,5 @@
 function main() {
     //d3 code goes here
-    d3.selectAll(".myclass").style("color", "navy");
+    //d3.selectAll(".myclass").style("color", "navy");
+    d3.select("tr").selectAll("td").style("background-color", "firebrick");
 }
